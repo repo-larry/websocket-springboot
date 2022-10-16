d27b8e782bb0dc06b072f0903373704a5f9489af
songyx <2250794569@qq.com>
2022-10-16 12:52:42 +0800

    websocket-demo

---
+++ socket-demo/src/main/java/com/jade/socket/demo/WebSocketServerConfigure.java
@@ -0,0 +1,24 @@
+package com.jade.socket.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+/**
+ * @description:
+ * @date: 2022/10/16
+ **/
+@Configuration
+@EnableWebSocket
+public class WebSocketServerConfigure implements WebSocketConfigurer {
+
+    @Autowired
+    private MyStringWebSocketHandler myStringWebSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(myStringWebSocketHandler, "/connect").withSockJS();
+    }
+}
